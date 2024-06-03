e410482e0ae54879111057e69386b27cad568a87
kiplimoboor <kiplimoarapboor@gmail.com>
2024-06-03 20:58:22 +0300

    unauthorized error

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,12 +2,13 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
 
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
+
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -19,6 +20,14 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """
+    Unaouthorized handler
+    """
+    return jsonify({"error": "Unauthorized"})
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
+from flask import abort, jsonify
+
 from api.v1.views import app_views
 
 
@@ -24,3 +25,13 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> str:
+    """
+    GET /api/v1/unauthorized
+    Return:
+        - an unaothorized error
+    """
+    abort(401)
