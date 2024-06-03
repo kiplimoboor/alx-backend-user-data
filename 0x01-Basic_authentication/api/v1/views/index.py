177733c803b16fa12100bc324074920dc8ca12e9
kiplimoboor <kiplimoarapboor@gmail.com>
2024-06-03 21:14:11 +0300

    forbidden error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -35,3 +35,13 @@ def unauthorized() -> str:
         - an unaothorized error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/forbidden
+    Return:
+        - a 403 forbidden error
+    """
+    abort(403)
