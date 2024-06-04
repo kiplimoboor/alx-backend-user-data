092d443f2bddc9a88b0193e5eeab8d46aeb3699b
kiplimoboor <kiplimoarapboor@gmail.com>
2024-06-04 10:01:29 +0300

    exclude auth paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,6 +18,14 @@ class Auth:
         Return:
             True if it requires auth, False otherwise
         """
+
+        if path is None or excluded_paths is None:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path not in excluded_paths or len(excluded_paths) == 0:
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
