146675e8a934c208ab1fd9a0a0b578b326c6d8cd
kiplimoboor <kiplimoarapboor@gmail.com>
2024-06-04 11:14:11 +0300

    basic auth implementation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,9 +15,13 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if getenv("AUTH_TYPE") == 'auth':
+AUTH_TYPE = getenv("AUTH_TYPE")
+if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Basic Authentication Module"""
+
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    A simple basic authentication class
+    """
+    pass
