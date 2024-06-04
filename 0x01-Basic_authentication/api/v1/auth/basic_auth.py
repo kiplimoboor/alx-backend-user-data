83a8ad7fe7e4f18fba4c7d111a960f22c5b0b93f
kiplimoboor <kiplimoarapboor@gmail.com>
2024-06-04 16:14:33 +0300

    split header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,16 @@ class BasicAuth(Auth):
     """
     A simple basic authentication class
     """
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of Authorization header
+        """
+
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+
+        header = authorization_header.split()
+        if header[0] != "Basic":
+            return None
+        return header[1]
