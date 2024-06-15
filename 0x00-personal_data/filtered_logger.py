34ddde0239893f8817a2c86e51328c5217932d69
kiplimoboor <kiplimoarapboor@gmail.com>
2024-06-15 21:48:31 +0300

    obfuscates sensitive user data

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""A simple logger that is filtered"""
+
+
+import re
+
+
+def filter_datum(fields: str, redaction: str,
+                 message: str, separator: str) -> str:
+    """Obfuscates personal data"""
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
