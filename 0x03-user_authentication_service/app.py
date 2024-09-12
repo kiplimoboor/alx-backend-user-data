48aa176da3fc7d54edc148dfcb45b0671adcddf0
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-12 22:10:18 +0300

    starter flask

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 
-from flask import Flask, jsonify
+from flask import Flask, Response, jsonify
 
 app = Flask(__name__)
 
 
-@app.get('/')
-def root():
-    return jsonify({"message": "Bienvenue"})
+@app.get('/', methods=['GET'])
+def root() -> Response:
+    """Root path for the app"""
+    msg = {"message": "Bienvenue"}
+    return jsonify(msg)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
