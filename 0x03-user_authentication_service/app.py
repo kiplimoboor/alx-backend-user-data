1389e8f4b8d548a47a9f32623c2f7d8f549128d7
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-12 23:07:59 +0300

    simple sign user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 
-from flask import Flask, Response, jsonify
+from flask import Flask, Response, jsonify, request
+
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -13,5 +16,19 @@ def root() -> str:
     return jsonify(msg)
 
 
+@app.route('/users', methods=['POST'])
+def users():
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    message = {"email": email, "message": "user created"}
+    return jsonify(message)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
