3370b5cf8967429cdfec7754bdb03b7bdd3a4d6f
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-12 12:38:20 +0300

    get user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,8 +3,9 @@
 """DB module
 """
 from sqlalchemy import create_engine, select
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import InvalidRequestError, NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
