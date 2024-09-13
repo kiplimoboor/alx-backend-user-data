89f5b0f6fb7cc45ed590465743cbc1e9e0e3f894
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-13 23:11:43 +0300

    reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -103,3 +103,18 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Destroys user session"""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        reset password tokem generation
+        Args
+            (email): email associated with account
+        Return:
+            uuid object that is token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
