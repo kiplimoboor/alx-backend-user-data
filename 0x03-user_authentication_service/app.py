693db524b8d8aa2d603afa96a7f832636cc4e45e
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-13 22:24:00 +0300

    logout app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,6 +49,8 @@ def login():
 @app.route('/sessions', methods=['DELETE'])
 def logout():
     session_id = request.cookies.get("session_id")
+    if session_id is None:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
