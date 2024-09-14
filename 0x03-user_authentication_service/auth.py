4e3fd5c0d43be1597f040c273414ef9960832fd8
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-14 08:41:54 +0300

    reset user pass

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -132,6 +132,6 @@ class Auth:
             self._db.update_user(
                 user.id,
                 hashed_password=_hash_password(password),
-                reset_token=reset_token)
+                reset_token=None)
         except NoResultFound:
             raise ValueError
